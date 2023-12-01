40eb97c364a73f8a6ba4e1392b195aa5df0850ea
Mandip Tamang <mandiptamang159@gmail.com>
2023-12-01 14:00:07 +0545

    update hero to Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import Hero from "./components/hero/hero"
-import Detail from "./components/hero/Detail"
-import Dealer from "./components/car dealer/Dealer"
-import Choose from "./components/car dealer/Choose"
-import Team from "./components/our team/Team"
-import Footer from "./components/header and footer/Footer"
+import Hero from './components/hero/Hero'
+import Detail from './components/hero/Detail'
+import Dealer from './components/car dealer/Dealer'
+import Choose from './components/car dealer/Choose'
+import Team from './components/our team/Team'
+import Footer from './components/header and footer/Footer'
 const App = () => {
   return (
     <div>
       <div className='w-[80vw] mx-auto max-w-6xl'>
-      
-        <Hero/>
-        <Detail/>
-        <Dealer/>
-        <Choose/>
-        <Team/>   
-      </div> 
-        <Footer/>
+        <Hero />
+        <Detail />
+        <Dealer />
+        <Choose />
+        <Team />
+      </div>
+      <Footer />
     </div>
   )
 }
