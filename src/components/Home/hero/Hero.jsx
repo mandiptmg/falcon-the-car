3692bb1f437acc29dd5f765bbbdf548e7b648d84
Mandip Tamang <mandiptamang159@gmail.com>
2024-01-03 11:15:27 +0545

    improve video section

--- src/components/Home/hero/Hero.jsx
+++ src/components/Home/hero/Hero.jsx
@@ -24,7 +24,7 @@ const hero = () => {
 
    useEffect(() => {
      const handleResize = () => {
-       const isMobile = window.innerWidth <= 800
+       const isMobile = window.innerWidth <= 768
        setStyle({
          ...style,
          width: isMobile ? '100%' : '70%',
